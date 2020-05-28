887ba4624f05be552acc8cf5142b4e3adb4c72e6
Francois Polo <andasan@gmail.com>
2020-05-28 10:57:48 -0700

    added session

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,15 +1,17 @@
 exports.getLogin = (req,res,next) => {
-    console.log(req.get('Cookie'));
-    const isLoggedIn  = req.get('Cookie').split('=')[1] === 'true'
-    console.log(isLoggedIn);
+    // console.log(req.get('Cookie'));
+    // const isLoggedIn  = req.get('Cookie').split('=')[1] === 'true'
+    // console.log(isLoggedIn);
+    console.log(req.session.isLoggedIn);
     res.render('auth/login', {
         path: '/auth/login',
         pageTitle: 'Login Page',
-        isAuth: isLoggedIn
+        isAuth: false
     })
 }
 
 exports.postLogin = (req,res,next) => {
-    res.setHeader('Set-Cookie', 'loggedIn=true'); 
+    // res.setHeader('Set-Cookie', 'loggedIn=true'); 
+    req.session.isLoggedIn = true;
     res.redirect('/');
 }
